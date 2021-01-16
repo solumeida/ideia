0c28199029c19f2ab1e035dd71f1119285502b7e
solumeida <solumeida@gmail.com>
2021-01-16 17:42:56 -0300

    Primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+function Home() {
+  return <div><h1>Eu sou a home-page</h1>  <Contador /></div>
+}
+
+function Contador() {
+  const [contador, setContador] = useState(1)
+  function add() {
+    setContador(contador + 1)      
+  }
+
+  return (
+    <div>
+      <div>{contador}</div>
+      <button onClick={add}>contador</button>
+      <h3>ReTeste!</h3>
+    </div>
+  )
+
+}
+
+export default Home
